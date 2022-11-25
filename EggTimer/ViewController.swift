ca22af8ff545f0e97daa2d58252caa317430b75e
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-25 15:03:40 +0100

    timer done, off to progress

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,25 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var hugeTopLabel: UILabel!
+    
+    //    let softTime = 5
+    //    let mediumTime = 7
+    //    let hardTime = 12
+    
+    let eggTimeInMinutes : [String:Int] = [
+        "Soft" : 5,
+        "Medium" : 7,
+        "Hard" : 12
+    ]
+    
+    var scheduledTimer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton){
         
         let hardness = sender.currentTitle
         
-        switch hardness {
-        case "Soft":
-            print (softTime)
-        case "Medium":
-            print (softTime)
-        case "Hard":
-            print (softTime)
-        default:
-            print ("There must be some typo ...")
+        var timeToBoilInSeconds = (eggTimeInMinutes[hardness ?? "Hard"] ?? 60) * 60
+        self.hugeTopLabel.text = "Your egg will boil \(timeToBoilInSeconds) seconds!"
+        scheduledTimer.invalidate()
+        
+        scheduledTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
+        {
+            (Timer) in
+            if timeToBoilInSeconds > 0 {
+                print ("\(timeToBoilInSeconds) seconds")
+                timeToBoilInSeconds -= 1
+            } else {
+                Timer.invalidate()
+                self.hugeTopLabel.text = "DONE!"
+            }
         }
         
     }
-
+    
 }
